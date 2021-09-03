b9f908bb65020c422cc0e55b4270a065f248de81
HongKim <kimxuanhong@outlook.com>
2021-09-03 18:49:18 +0700

    Apply Log4j2

--- src/main/java/com/xhk/mtv/logger/LoggingService.java
+++ src/main/java/com/xhk/mtv/logger/LoggingService.java
@@ -1,7 +1,10 @@
 package com.xhk.mtv.logger;
 
 
-import org.apache.log4j.Logger;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,47 +17,52 @@ import java.util.List;
 
 @Component
 public class LoggingService {
-	private static final Logger logger = Logger.getLogger(LoggingService.class);
 
-	public void logRequest(HttpServletRequest httpServletRequest, Object body) {
-		List<LogHeader> headers = buildHeadersMap(httpServletRequest);
-		LogHttpRequestEntity entity = new LogHttpRequestEntity();
-		entity.setType("http-request");
-		entity.setMethod(httpServletRequest.getMethod());
-		entity.setHeaders(headers);
-		entity.setPath(httpServletRequest.getRequestURI());
-		entity.setBody(body);
-		logger.info(new LogMessage("Request", entity));
-	}
+    private static final Logger logger = LogManager.getLogger(LoggingService.class);
 
-	public void logResponse(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object body) {
-		List<LogHeader> headers = buildHeadersMap(httpServletResponse);
-		LogHttpResponseEntity entity = new LogHttpResponseEntity();
-		entity.setType("http-response");
-		entity.setMethod(httpServletRequest.getMethod());
-		entity.setHeaders(headers);
-		entity.setPath(httpServletRequest.getRequestURI());
-		entity.setBody(body);
-		logger.info(new LogMessage("Response", entity));
-	}
+    private final Gson gson = (new GsonBuilder()).create();
 
-	private List<LogHeader> buildHeadersMap(HttpServletRequest request) {
-		List<LogHeader> list = new ArrayList<>();
-		Enumeration<String> headerNames = request.getHeaderNames();
-		while (headerNames.hasMoreElements()) {
-			String key = headerNames.nextElement();
-			String value = request.getHeader(key);
-			list.add(new LogHeader(key, value));
-		}
-		return list;
-	}
-	
-	private List<LogHeader> buildHeadersMap(HttpServletResponse response) {
-		List<LogHeader> list = new ArrayList<>();
-		Collection<String> headerNames = response.getHeaderNames();
-		for (String header : headerNames) {
-			list.add(new LogHeader(header, response.getHeader(header)));
-		}
-		return list;
-	}
+    public void logRequest(HttpServletRequest httpServletRequest, Object body) {
+        List<LogHeader> headers = buildHeadersMap(httpServletRequest);
+        LogHttpRequestEntity entity = new LogHttpRequestEntity();
+        entity.setType("http-request");
+        entity.setMethod(httpServletRequest.getMethod());
+        entity.setHeaders(headers);
+        entity.setPath(httpServletRequest.getRequestURI());
+        entity.setBody(body);
+
+        logger.info("{}", gson.toJson( new LogMessage("Request", entity)));
+    }
+
+    public void logResponse(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object body) {
+        List<LogHeader> headers = buildHeadersMap(httpServletResponse);
+        LogHttpResponseEntity entity = new LogHttpResponseEntity();
+        entity.setType("http-response");
+        entity.setMethod(httpServletRequest.getMethod());
+        entity.setHeaders(headers);
+        entity.setPath(httpServletRequest.getRequestURI());
+        entity.setBody(body);
+
+        logger.info("{}", gson.toJson( new LogMessage("Response", entity)));
+    }
+
+    private List<LogHeader> buildHeadersMap(HttpServletRequest request) {
+        List<LogHeader> list = new ArrayList<>();
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = headerNames.nextElement();
+            String value = request.getHeader(key);
+            list.add(new LogHeader(key, value));
+        }
+        return list;
+    }
+
+    private List<LogHeader> buildHeadersMap(HttpServletResponse response) {
+        List<LogHeader> list = new ArrayList<>();
+        Collection<String> headerNames = response.getHeaderNames();
+        for (String header : headerNames) {
+            list.add(new LogHeader(header, response.getHeader(header)));
+        }
+        return list;
+    }
 }
